7856848d7f26b05f596d7875bf03d3d9641afdb1
sneweyy <36225076+sneweyy@users.noreply.github.com>
2018-04-11 12:02:41 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -171,3 +171,21 @@ Highcharts.chart('container', {
         }
     }]
 });
+
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
